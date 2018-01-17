97020303c99eccca98d0de41d4c44822fec584a0
nfinke <nicolai.finke@gmail.com>
2018-01-17 21:25:33 +0100

    Registered service worker and some event handler. Added manifest.json for working pwa on Android

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,3 +1,18 @@
+// 1) Register a Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(function() {
+      console.log('Service worker registered');
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired ...');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+})
 
 var box = document.querySelector('.box');
 var button = document.querySelector('button');
@@ -8,9 +23,27 @@ button.addEventListener('click', function(event) {
   } else {
     box.classList.add('visible');
   }
+
+  if(deferredPrompt){
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(function(choiceResult){
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed'){
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+
+      deferredPrompt = null;
+
+    });
+  }
+
+
 });
 
-// 1) Register a Service Worker
+
 // 2) Identify the AppShell (i.e. core assets your app requires to provide its basic "frame")
 // 3) Precache the AppShell
 // 4) Add Code to fetch the precached assets from cache when needed
