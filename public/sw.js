cb2115dcc1d2fe7995a481ad1654cabf1a6015c4
nfinke <nicolai.finke@gmail.com>
2018-03-11 21:40:00 +0100

    Added dynmic caching and cache versioning

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
-      caches.open('static')
+      caches.open(CACHE_STATIC_NAME)
         .then(function(cache) {
           console.log('[Service Worker] Precaching App Shell');
           cache.addAll([
@@ -9,7 +12,6 @@ self.addEventListener('install', function(event) {
               '/index.html',
               '/src/js/main.js',
               '/src/js/material.min.js',
-              '/src/css/main.css',
               '/src/css/app.css',
               '/src/css/main.css',
               'https://fonts.googleapis.com/css?family=Roboto:400,700',
@@ -20,8 +22,21 @@ self.addEventListener('install', function(event) {
     );
 });
 
+// only called after the user closed the application so we can be sure
+// there is no one still around to access the cache
 self.addEventListener('activate', function(event) {
     console.log('[Service Worker] Activating Service Worker ...', event);
+    event.waitUntil(
+      caches.keys()
+        .then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+              if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                console.log('[Service Worker] Removing old cache.', key);
+                return caches.delete(key);
+              }
+            }));
+        })
+    );
     return self.clients.claim();
 });
 
@@ -35,11 +50,14 @@ self.addEventListener('fetch', function(event) {
         } else {
           return fetch(event.request)
             .then(function(res){
-              return caches.open('dynamic')
+              return caches.open(CACHE_DYNAMIC_NAME)
                 .then(function(cache){
                     cache.put(event.request.url, res.clone())
                     return res;
                 })
+            })
+            .catch(function(){
+
             });
         }
       })
